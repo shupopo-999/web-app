27f3cbfeea6b7a2e1be03c5f42f04dd9e973e43f
shupopo-999 <shu999.20099@gmail.com>
2024-12-19 23:31:09 +0900

    add: jsonの追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,80 +1,13 @@
 import './App.css';
 import React, { useState } from 'react';
 import Countdown from './countdown';
+import Quiz from './quiz.json';
 
-function App() {
-    const quiz = [
-    {
-      id: 1,
-      question: "message",
-      ansewer: ["メッセージ", "めっせーじ","messe-ji","messe-zi","伝言","でんごん","denngonn","dengon"]
-    },
-    {
-      id: 2,
-      question: "apple",
-      ansewer: ["りんご", "リンゴ","rinngo","ringo"]
-    },
-    {
-      id: 3,
-      question: "consensus",
-      ansewer: ["コンセンサス", "こんせんさす", "konsensasu","konnsennsasu","一致","いっち","itti", "合意","ごうい","goui"]
-    },
-    {
-      id: 4,
-      question: "spark",
-      ansewer: ["火花","ひばな","hibana","きらめき","kiramenki"]
-    },
-    {
-      id: 5,
-      question: "speak",
-      ansewer: ["話す","はなす","hanasu","しゃべる","shaberu"]
-    },
-    {
-      id: 6,
-      question: "success",
-      ansewer: ["成功","せいこう","seikou"]
-    },
-    {
-      id: 7,
-      question: "decay",
-      ansewer: ["腐る","くさる","kusaru"]
-    },
-    {
-      id: 8,
-      question: "作る",
-      ansewer: ["make up","Make Up","make","Make"]
-    },
-    {
-      id: 9,
-      question: "破壊",
-      ansewer: ["break","Break","destruction","やぶる","yaburu"]
-    },
-    {
-      id: 10,
-      question: "虚数",
-      ansewer: ["imaginary part","Imaginary Part"]
-    },
-    {
-      id: 11,
-      question: "価格",
-      ansewer: ["Price","price","Prices","prices","Value","value"]
-    },
-    {
-      id: 12,
-      question: "体験する",
-      ansewer: ["Experience it","experience it"]
-    },
-    {
-      id: 13,
-      question: "説明",
-      ansewer: ["Description","explanation","Explanation"]
-    }
-  ];
-  
+function App() {  
   const [inputValue, setInputValue] = useState("");
-  const [quizIndex,setQuizIndex] = useState(Math.floor(Math.random() * quiz.length));
+  const [quizIndex,setQuizIndex] = useState(Math.floor(Math.random() * Quiz.quiz.length));
   const [result, setResult] = useState("");
-  const [timeInedx,setTime] = useState(60);
+  const [timeInedx] = useState(60);
 
   
 
@@ -87,12 +20,15 @@ function App() {
     setInputValue(e.target.value);
   }
 
+  const setExpl = () => {
+    return Quiz.quiz[quizIndex].explanation;
+  }
   const setQuiz = () => {
-    return quiz[quizIndex].question;
+    return Quiz.quiz[quizIndex].question;
   }
     
   const judgment = () =>{
-    if(quiz[quizIndex].ansewer.includes(inputValue)){
+    if(Quiz.quiz[quizIndex].ansewer.includes(inputValue)){
       setResult("正解");
       updateQuiz();
     }else{
@@ -104,7 +40,7 @@ function App() {
   const updateQuiz = () =>{
     let nextIndex;
     do{
-      nextIndex = Math.floor((Math.random() * quiz.length));
+      nextIndex = Math.floor((Math.random() * Quiz.quiz.length));
     }while(nextIndex === quizIndex);
     if(timeInedx !== 0){
       setQuizIndex(nextIndex);
@@ -113,19 +49,20 @@ function App() {
 
   return (
     <div className="App">
-      <h3>
-        <Countdown time={timeInedx}/>
-        <h2>{setQuiz()}</h2>
-        <ul>{result}</ul>
+      <p>
+        <h1><Countdown time={timeInedx}/></h1>
+        <p>{setExpl()}</p>
+        <h1>{setQuiz()}</h1>
         <form onSubmit={(e) => handlesubmit(e)} > 
           <input
             type="text"
             value={inputValue}
             onChange={(e) => handlechenge(e)}
             className="inputText"
-          />
+            />
         </form>
-      </h3>
+      </p>
+      <h1>{result}</h1>
     </div>
   );
 }
